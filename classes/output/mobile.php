11c8da30570ca559b9c68dda405698544db2ca1a
Jakob Heinemann <jakob@jakobheinemann.de>
2022-03-12 16:47:22 +0100

    version bump

---
+++ classes/output/mobile.php
@@ -0,0 +1,51 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Mobile output class for qbehaviour_deferredfeedbackstudentfb
+ *
+ * @package    qbehaviour_deferredfeedbackstudentfb
+ * @copyright  2022 Jakob Heinemann
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace qbehaviour_deferredfeedbackstudentfb\output;
+
+
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Mobile output class for qbehaviour_deferredfeedbackstudentfb
+ *
+ * @package    qbehaviour_deferredfeedbackstudentfb
+ * @copyright  2022 Jakob Heinemann
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class mobile {
+
+    /**
+     * Returns the deferredfeedbackstudentfb question behaviour for the quiz in the mobile app.
+     *
+     * @return void
+     */
+    public static function mobile_get_deferredfeedbackstudentfb($args) {
+        global $CFG;
+        $args = (object) $args;
+        
+        return [];
+    }
+}
